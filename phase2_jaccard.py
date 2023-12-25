e35028cab1a4e5caf9dee33d43ff7d592aafff6b
Long3309 <20520239@gm.uit.edu.vn>
2023-12-25 11:27:43 +0700

    commit all

--- phase2_jaccard.py
+++ phase2_jaccard.py
@@ -42,18 +42,6 @@ class MRRecommendation(MRJob):
         yield item_pair, jaccard_similarity
 
 
-class MRRecommendation2(MRJob):
-    def steps(self):
-        return [
-            MRStep(mapper=self.mapper_preprocess, reducer=self.reducer_preprocess)
-        ]
-    # Phase 1: Preprocessing
-    def mapper_preprocess(self, _, line):
-        userId, *item_ratings = line.split('\t')
-        yield userId, [(item, rating) for item, rating in zip(item_ratings[::2], item_ratings[1::2])]
-
-    def reducer_preprocess(self, userId, item_rating_lists):
-        yield userId, [item_rating for sublist in item_rating_lists for item_rating in sublist]
 
 if __name__ == '__main__':
     MRRecommendation.run()
